4e59e1f015b84cb7033abe9dc25920a0525b21a1
Jae Lee <jaequery@gmail.com>
2022-10-08 02:45:51 -0700

    chore: added monorepo w/ Nest and Next

---
+++ packages/api/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
+
+@Module({
+  imports: [
+    ConfigModule.register({
+      folder: './config',
+      environment: process.env.NODE_ENV || 'development',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
